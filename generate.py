81af092f4947f3d132b24b6d98d932e67ed88d08
Said Azizov <stazizov@sberbank.ru>
2023-10-15 05:55:45 +0300

    integrated model

---
+++ generate.py
@@ -0,0 +1,147 @@
+N_CORES = 128 # Где N_CORES - количество ядер CPU
+
+import os
+
+os.environ["MKL_NUM_THREADS"] = str(N_CORES)
+
+os.environ["NUMEXPR_NUM_THREADS"] = str(N_CORES)
+
+os.environ["OMP_NUM_THREADS"] = str(N_CORES)
+from diffusers import KandinskyV22PriorPipeline, KandinskyV22Pipeline, KandinskyV22PriorEmb2EmbPipeline, KandinskyV22ControlnetImg2ImgPipeline, KandinskyV22Img2ImgPipeline
+import torch
+from PIL import Image
+from PIL import ImageDraw, ImageFont
+from copy import deepcopy
+from deep_translator import GoogleTranslator
+
+from rembg import remove
+import sys
+sys.path.append('/home/jovyan/cene655/ControlNet')
+from annotator.util import resize_image, HWC3
+from annotator.midas import MidasDetector
+from diffusers import KandinskyV22PriorEmb2EmbPipeline
+from diffusers import KandinskyV22ControlnetImg2ImgPipeline
+import torch
+import PIL
+import torch
+from diffusers.utils import load_image
+from torchvision import transforms
+from transformers import CLIPVisionModelWithProjection
+from diffusers.models import UNet2DConditionModel
+import numpy as np
+import imageio.v3 as iio
+
+def make_hint(img, detector):
+    input_image = np.array(img)
+    img = HWC3(input_image)#resize_image(, input_image.shape[1])
+    print(img.shape)
+    H, W, C = img.shape
+    detected_map, _ = detector(img)
+    detected_map = HWC3(detected_map)
+    detected_map = torch.from_numpy(detected_map.copy()).float() / 255.0
+    hint = detected_map.permute(2, 0, 1)
+    return hint
+
+detector = MidasDetector()
+decoder = KandinskyV22Img2ImgPipeline.from_pretrained('kandinsky-community/kandinsky-2-2-decoder', torch_dtype=torch.float16).to('cuda')
+controlnet = KandinskyV22ControlnetImg2ImgPipeline.from_pretrained('kandinsky-community/kandinsky-2-2-controlnet-depth', torch_dtype=torch.float16).to('cuda')
+prior = KandinskyV22PriorPipeline.from_pretrained('kandinsky-community/kandinsky-2-2-prior', torch_dtype=torch.float16).to('cuda')
+
+prompt = 'pizza in the space, photo'
+def prepare_caption(caption): return GoogleTranslator(source='ru', target='en').translate(caption)
+
+def delete_bg(img, img2):
+    min_size = min(img2.size[0], img2.size[1])
+
+    img = img.resize((min_size, min_size))
+    img = np.array(img)
+    img2 = np.array(img2)
+    output = remove(img)
+    m = 0
+    for i in range(output.shape[0]):
+        for j in range(output.shape[1]):
+            m = max(output[i][j][3], m)
+            if output[i][j][3] >= 80:
+                img2[i][j] = output[i][j][:3]
+    return Image.fromarray(img2)
+
+def generate_avatar(decoder, controlnet, prior, prompt, text=None, style_image=None, face_image=None, animation=None):
+        
+    zero_img = PIL.Image.new(mode="RGB", size=(200, 200))
+    img_emb = prior(prompt=prompt, num_inference_steps=50, num_images_per_prompt=1, guidance_scale=1.0).image_embeds
+    if style_image is not None:
+        img_emb2 = prior.interpolate(images_and_prompts=[style_image], weights=[1.0], num_inference_steps=25, num_images_per_prompt=1, guidance_scale=4.0).image_embeds
+        img_emb = img_emb * 0.55 + img_emb2 * 0.45
+    negative_emb = prior(prompt='blow quility', num_inference_steps=25, num_images_per_prompt=1, guidance_scale=4.0)
+    images = decoder(image=zero_img, strength=1.0, image_embeds=img_emb, negative_image_embeds=negative_emb.negative_image_embeds, num_inference_steps=100, height=832, width=832, guidance_scale=4.0).images
+    if face_image is not None:
+        images[0] = delete_bg(face_image, images[0])
+    if text is not None:
+        img = deepcopy(images[0])
+        W, H = img.size
+        myFont = ImageFont.truetype('cene655/drawbench/DejaVuSansCondensed.ttf', 40)
+
+        draw = ImageDraw.Draw(img)
+        _, _, w, h = draw.textbbox((0, 0), text, font=myFont)
+        draw.text(((W-w)/2, 25), text, font=myFont, fill=(255, 255, 255), stroke_width=2, stroke_fill=(0, 0, 0))
+        hint = make_hint(img, detector).unsqueeze(0).half().to('cuda').repeat(1, 1, 1, 1)
+        images2 = controlnet(image=img, strength=0.05, image_embeds=img_emb, negative_image_embeds=negative_emb.image_embeds, hint=hint, num_inference_steps=100, height=img.size[1], width=img.size[0])
+        return images2[0][0]
+    else:
+        return images[0]
+
+    
+def generate_preview(decoder, contronet, prior, prompt, text=None, style_image=None, face_image=None, animation=None):
+    prompt = '4k photo of ' + prompt
+    zero_img = PIL.Image.new(mode="RGB", size=(200, 200))
+    img_emb = prior(prompt=prompt, num_inference_steps=50, num_images_per_prompt=1, guidance_scale=1.5).image_embeds
+    if style_image is not None:
+        img_emb2 = prior.interpolate(images_and_prompts=[style_image], weights=[1.0], num_inference_steps=25, num_images_per_prompt=1, guidance_scale=4.0).image_embeds
+        img_emb = img_emb * 0.75 + img_emb2 * 0.25
+    negative_emb = prior(prompt='blow quility', num_inference_steps=25, num_images_per_prompt=1, guidance_scale=4.0)
+    images = decoder(image=zero_img, strength=1.0, image_embeds=img_emb, negative_image_embeds=negative_emb.negative_image_embeds, num_inference_steps=250, height=704, width=1280, guidance_scale=4.0).images
+    if face_image is not None:
+        images[0] = delete_bg(face_image, images[0])
+    if text is not None:
+        img = deepcopy(images[0])
+        W, H = img.size
+        print(len(text))
+        if len(text) < 52:
+            myFont = ImageFont.truetype('cene655/drawbench/DejaVuSansCondensed.ttf', 50)
+        else:
+            myFont = ImageFont.truetype('cene655/drawbench/DejaVuSansCondensed.ttf', 35)
+        draw = ImageDraw.Draw(img)
+        _, _, w, h = draw.textbbox((0, 0), text, font=myFont)
+        draw.text(((W-w)/2, 25), text, font=myFont, fill=(255, 255, 255), stroke_width=2, stroke_fill=(0, 0, 0))
+        hint = make_hint(img, detector).unsqueeze(0).half().to('cuda').repeat(1, 1, 1, 1)
+        #images2 = controlnet(image=img, strength=0.01, image_embeds=img_emb, negative_image_embeds=negative_emb.image_embeds, hint=hint, num_inference_steps=150, height=img.size[1], width=img.size[0])
+        return img#images2[0][0]
+    else:
+        return images[0]
+
+def generate_banner(decoder, controlnet, prior, prompt, text=None, style_image=None, face_image=None, animation=None):
+    prompt = '4k photo of ' + prompt
+    zero_img = PIL.Image.new(mode="RGB", size=(200, 200))
+    img_emb = prior(prompt=prompt, num_inference_steps=50, num_images_per_prompt=1, guidance_scale=1.5).image_embeds
+    if style_image is not None:
+        img_emb2 = prior.interpolate(images_and_prompts=[style_image], weights=[1.0], num_inference_steps=25, num_images_per_prompt=1, guidance_scale=4.0).image_embeds
+        img_emb = img_emb * 0.75 + img_emb2 * 0.25
+    negative_emb = prior(prompt='blow quility', num_inference_steps=25, num_images_per_prompt=1, guidance_scale=4.0)
+    images = decoder(image=zero_img, strength=1.0, image_embeds=img_emb, negative_image_embeds=negative_emb.negative_image_embeds, num_inference_steps=100, height=896, width=2240, guidance_scale=4.0).images
+    if face_image is not None:
+        images[0] = delete_bg(face_image, images[0])
+    if text is not None:
+        img = deepcopy(images[0])
+        W, H = img.size
+        if len(text) < 52:
+            myFont = ImageFont.truetype('cene655/drawbench/DejaVuSansCondensed.ttf', 100)
+        else:
+            myFont = ImageFont.truetype('cene655/drawbench/DejaVuSansCondensed.ttf', 35)
+        draw = ImageDraw.Draw(img)
+        _, _, w, h = draw.textbbox((0, 0), text, font=myFont)
+        draw.text(((W-w)/2, (H-h)/2), text, font=myFont, fill=(255, 255, 255), stroke_width=2, stroke_fill=(0, 0, 0))
+        hint = make_hint(img, detector).unsqueeze(0).half().to('cuda').repeat(1, 1, 1, 1)
+        images2 = controlnet(image=img, strength=0.05, image_embeds=img_emb, negative_image_embeds=negative_emb.image_embeds, hint=hint, num_inference_steps=150, height=img.size[1], width=img.size[0])
+        return images2[0][0].resize((2204, 864))
+    else:
+        return images[0].resize((2204, 864))
--- gui.py
+++ gui.py
@@ -1,7 +1,19 @@
+import imageio.v3 as iio
 import streamlit as st
 from PIL import Image
 import numpy as np
+import cv2
 import io
+
+# from generate import (
+#     generate_avatar, 
+#     generate_preview, 
+#     generate_banner,
+#     detector, 
+#     decoder, 
+#     controlnet, 
+#     prior
+# )
 
 animation_options = [
     None,
@@ -17,120 +29,216 @@ animation_options = [
     'live'
 ]
 
-def create_tab_1_2(tab_key, image_size):
-    imageLocation = st.empty()
-    st.session_state[f"{tab_key}_text"] = st.text_input(
-        "текст на баннере", 
-        value="Название канала", 
-        max_chars=50, 
-        key=f"{tab_key}_text_input"
-        )
-    st.session_state[f"{tab_key}_prompt"] = st.text_input(
-        "промпт для генерации баннера", 
-        key=f"{tab_key}_prompt_text"
-        )
-    st.selectbox(
-        "Анимация", 
-        options=animation_options, 
-        index=0, 
-        key=f"{tab_key}_animation_selectbox"
-        )
+def generate_image(prompt, text, user_image, image_size=(768, 768)):
+    return Image.open("test.jpg").resize(image_size)
+
+def generate_video():
+    return Image.open("test.mp4")    
+        
+
+if __name__ == "__main__":
+    user_data = dict()
+    tab1, tab2, tab3 = st.tabs(["Баннер 🌇", "Аватар 🗿", "Превью видео 🎥"])
+
+    with tab1:
+        tab_key = "banner"
+        st.title("Генерация Баннера 🌇")
+
+        st.session_state[f"{tab_key}_imageLocation"] = st.empty()
+        st.session_state[f"{tab_key}_text"] = st.text_input(
+            "текст на баннере", 
+            value="Название канала", 
+            max_chars=50, 
+            key=f"{tab_key}_text_input"
+            )
+        st.session_state[f"{tab_key}_prompt"] = st.text_input(
+            "промпт для генерации баннера", 
+            key=f"{tab_key}_prompt_text"
+            )
+        st.selectbox(
+            "Анимация", 
+            options=animation_options, 
+            index=0, 
+            key=f"{tab_key}_animation_selectbox"
+            )
+        
+        init_image_upload = st.file_uploader(
+            "Загрузи фото для стилизации (опционально)", 
+            type=["png", "jpg", "jpeg"], 
+            accept_multiple_files=False,
+            key=f"{tab_key}_init_image_upload"
+            )
+
+        if init_image_upload is not None:
+            st.session_state[f"{tab_key}_init_image"] = Image.open(
+                io.BytesIO(init_image_upload.getvalue())
+                )
+        else: 
+            st.session_state[f"{tab_key}_init_image"] = Image.fromarray(
+                (np.random.random(size=(800, 800, 3)) * 255).astype(np.uint8)
+                )
+
+        st.session_state[f"{tab_key}_imageLocation"].image(
+            st.session_state[f"{tab_key}_init_image"], 
+            width=800
+            )
+
+        user_image_upload = st.file_uploader(
+            "Загрузи свое фото (опционально)", 
+            type=["png", "jpg", "jpeg"], 
+            accept_multiple_files=False,
+            key=f"{tab_key}_user_image_upload"
+            )
+        if user_image_upload:
+            st.session_state[f"{tab_key}_user_image"] = Image.open(
+                io.BytesIO(user_image_upload.getvalue())
+            )
+        else: 
+            st.session_state[f"{tab_key}_user_image"] = None
+        
+        if st.button(label="Сгенерировать!", key=f"{tab_key}_button"):
+            # image = generate_avatar(
+            #     decoder, controlnet, prior, 
+            #     st.session_state["preview_prompt"], 
+            #     st.session_state["preview_text"], 
+            #     st.session_state["preview_init_image"], 
+            #     st.session_state["preview_user_image_upload"], 
+            #     st.session_state["preview_animation_selectbox"], 
+            # )
+            image = Image.fromarray(np.ones((800, 800, 3), dtype=np.uint8) * np.random.randint(0, 255)) 
+            st.session_state[f"{tab_key}_imageLocation"].image(image)
+
+    with tab2:
+        tab_key = "avatar"
+        st.title("Генерация Аватара 🗿")
+        
+
+        st.session_state[f"{tab_key}_imageLocation"] = st.empty()
+        st.session_state[f"{tab_key}_text"] = st.text_input(
+            "текст на баннере", 
+            value="Название канала", 
+            max_chars=50, 
+            key=f"{tab_key}_text_input"
+            )
+        st.session_state[f"{tab_key}_prompt"] = st.text_input(
+            "промпт для генерации баннера", 
+            key=f"{tab_key}_prompt_text"
+            )
+        st.selectbox(
+            "Анимация", 
+            options=animation_options, 
+            index=0, 
+            key=f"{tab_key}_animation_selectbox"
+            )
+        
+        init_image_upload = st.file_uploader(
+            "Загрузи фото для стилизации (опционально)", 
+            type=["png", "jpg", "jpeg"], 
+            accept_multiple_files=False,
+            key=f"{tab_key}_init_image_upload"
+            )
+
+        if init_image_upload is not None:
+            st.session_state[f"{tab_key}_init_image"] = Image.open(
+                io.BytesIO(init_image_upload.getvalue())
+                )
+        else: 
+            st.session_state[f"{tab_key}_init_image"] = Image.fromarray(
+                (np.random.random(size=(800, 800, 3)) * 255).astype(np.uint8)
+                )
+
+        st.session_state[f"{tab_key}_imageLocation"].image(
+            st.session_state[f"{tab_key}_init_image"], 
+            width=800
+            )
+
+        user_image_upload = st.file_uploader(
+            "Загрузи свое фото (опционально)", 
+            type=["png", "jpg", "jpeg"], 
+            accept_multiple_files=False,
+            key=f"{tab_key}_user_image_upload"
+            )
+        if user_image_upload:
+            st.session_state[f"{tab_key}_user_image"] = Image.open(
+                io.BytesIO(user_image_upload.getvalue())
+            )
+        else: 
+            st.session_state[f"{tab_key}_user_image"] = None
+        
+        if st.button(label="Сгенерировать!", key=f"{tab_key}_button"):
     
-    init_image_upload = st.file_uploader(
-        "Загрузи фото для стилизации (опционально)", 
-        type=["png", "jpg", "jpeg"], 
-        accept_multiple_files=False,
-        key=f"{tab_key}_init_image_upload"
-        )
+            # image = generate_avatar(
+            #     decoder, controlnet, prior, 
+            #     st.session_state[f"{tab_key}_prompt"], 
+            #     st.session_state[f"{tab_key}_text"], 
+            #     st.session_state[f"{tab_key}_init_image"], 
+            #     st.session_state[f"{tab_key}_user_image_upload"], 
+            #     st.session_state[f"{tab_key}_animation_selectbox"], 
+            # )
+            image = Image.fromarray(np.ones((800, 800, 3), dtype=np.uint8) * np.random.randint(0, 255))
+            st.session_state[f"{tab_key}_imageLocation"].image(image)
+            
 
-    if init_image_upload is not None:
-        st.session_state[f"{tab_key}_init_image"] = Image.open(
-            io.BytesIO(init_image_upload.getvalue())
+    with tab3:
+        tab_key = "preview"
+        st.session_state[f"{tab_key}_imageLocation"] = st.empty()
+        st.title("Генерация Превью видео 🎥")
+        imageLocation = st.empty()
+        st.session_state[f"{tab_key}_text"] = st.text_input(
+            "текст на баннере", 
+            value="Название канала", 
+            max_chars=50, 
+            key=f"{tab_key}_text_input"
             )
-    else: 
-        st.session_state[f"{tab_key}_init_image"] = Image.fromarray(
-            (np.random.random(size=image_size) * 255).astype(np.uint8)
-            )
-
-    imageLocation.image(
-        st.session_state[f"{tab_key}_init_image"], 
-        width=image_size[1]
-        )
-
-    user_image_upload = st.file_uploader(
-        "Загрузи свое фото (опционально)", 
-        type=["png", "jpg", "jpeg"], 
-        accept_multiple_files=False,
-        key=f"{tab_key}_user_image_upload"
-        )
-    if user_image_upload:
-        st.session_state[f"{tab_key}_user_image"] = Image.open(
-            io.BytesIO(user_image_upload.getvalue())
-        )
-    else: 
-        st.session_state[f"{tab_key}_user_image"] = None
-
-def create_tab_3(tab_key, image_size):
-    imageLocation = st.empty()
-    st.session_state[f"{tab_key}_text"] = st.text_input(
-        "текст на баннере", 
-        value="Название канала", 
-        max_chars=50, 
-        key=f"{tab_key}_text_input"
-        )
-    st.session_state[f"{tab_key}_prompt"] = st.text_input(
-        "промпт для генерации баннера", 
-        key=f"{tab_key}_prompt_text"
-        )
-
-    st.selectbox(
-        "Анимация", 
-        options=animation_options, 
-        index=0, 
-        key=f"{tab_key}_animation_selectbox"
-        )
-    
-    init_image_upload = st.file_uploader(
-        "Загрузи видео", 
-        type=["mp4"], 
-        accept_multiple_files=False,
-        key=f"{tab_key}_init_image_upload"
-        )
-
-    if init_image_upload is not None:
-        st.session_state[f"{tab_key}_init_image"] = Image.open(
-            io.BytesIO(init_image_upload.getvalue())
-            )
-    else: 
-        st.session_state[f"{tab_key}_init_image"] = Image.fromarray(
-            (np.random.random(size=image_size) * 255).astype(np.uint8)
+        st.session_state[f"{tab_key}_prompt"] = st.text_input(
+            "промпт для генерации баннера", 
+            key=f"{tab_key}_prompt_text"
             )
 
-
-    imageLocation.image(
-        st.session_state[f"{tab_key}_init_image"], 
-        width=image_size[1]
+        st.selectbox(
+            "Анимация", 
+            options=animation_options, 
+            index=0, 
+            key=f"{tab_key}_animation_selectbox"
+            )
+        
+        init_video_upload = st.file_uploader(
+            "Загрузи видео", 
+            type=["mp4"], 
+            accept_multiple_files=False,
+            key=f"{tab_key}_init_image_upload"
         )
 
+        frame_skip = 1 # display every 300 frames
 
-user_data = dict()
-tab1, tab2, tab3 = st.tabs(["Баннер 🌇", "Аватар 🗿", "Превью видео 🎥"])
+        if init_video_upload is not None: # run only when user uploads video
+            if init_video_upload is not None: # run only when user uploads video
+                vid = init_video_upload.name
+                with open(vid, mode='wb') as f:
+                    f.write(init_video_upload.read()) # save video to disk
+                    
+                vidcap = cv2.VideoCapture(vid) # load video from disk
+                cur_frame = 0
+                success = True
+                max_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-with tab1:
-    tab_key = "banner"
-    st.title("Генерация Баннера 🌇")
-    create_tab_1_2(tab_key, (400, 800, 3))
-    st.button("Сгенерировать!", key=f"{tab_key}_generate_button", on_click=None)
+                while success:
+                    success, frame = vidcap.read() # get next frame from video
+                    if cur_frame <= max_frames//2 and success: # only analyze every n=300 frames
+                        if cur_frame != max_frames//2: 
+                            continue
+                        pil_img = Image.fromarray(frame[:, :, ::-1]) # convert opencv frame (with type()==numpy) into PIL Image
+                        st.session_state[f"{tab_key}_init_image"] = pil_img
+                        imageLocation.image(pil_img)
+                    cur_frame += 1
 
-with tab2:
-    tab_key = "avatar"
-    st.title("Генерация Аватара 🗿")
-    create_tab_1_2(tab_key, (800, 800, 3))
-    
-    st.button("Сгенерировать!", key=f"{tab_key}_generate_button", on_click=None)
-
-with tab3:
-    tab_key = "preview"
-    st.title("Генерация Превью видео 🎥")
-    create_tab_3(tab_key, (400, 800, 3))
-    st.button("Сгенерировать!", key=f"{tab_key}_generate_button", on_click=None)
+        if st.button(label="Сгенерировать!", key=f"{tab_key}_button"):
+            # image = generate_avatar(
+            #     decoder, controlnet, prior, 
+            #     st.session_state[f"{tab_key}_prompt"], 
+            #     st.session_state[f"{tab_key}_text"], 
+            #     st.session_state[f"{tab_key}_init_image"], 
+            #     st.session_state[f"{tab_key}_animation_selectbox"], 
+            # )
+            image = Image.fromarray(np.ones((800, 800, 3), dtype=np.uint8) * np.random.randint(0, 255))
+            st.session_state[f"{tab_key}_imageLocation"].image(image)
